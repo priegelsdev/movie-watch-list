7b0f61e66138cf5dd2e471b1bc9edb9c9e1b4705
priegelsdev <priegelsdev@gmail.com>
2023-01-11 13:22:45 +0100

    wire up basic fetch api fn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=28f05d61
+
+const searchInput = document.querySelector('.search-input')
+const searchBtn = document.querySelector('.search-btn')
+
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  getApi();
+})
+
+async function getApi() {
+  const searchValue = searchInput.value
+  console.log(searchValue)
+  const res = await fetch(`https://www.omdbapi.com/?t=${searchValue}&apikey=28f05d61`)
+  const data = await res.json()
+
+
+  console.log(data)
+}
